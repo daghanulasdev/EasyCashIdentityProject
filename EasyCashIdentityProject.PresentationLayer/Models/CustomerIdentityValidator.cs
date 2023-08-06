3ea7483f51e8bed516bbfd7e69bd58ed56b8eee3
pxr <pxr@DESKTOP-BIP5GPD>
2023-08-06 10:19:23 +0300

    Identity Error Describer Model

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomerIdentityValidator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomerIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",
+				Description = $"Parola en az {length} karakter olmalidir"
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Lutfen en az 1 buyuk harf giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresLower() 
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Lutfen en az 1 kucuk harf giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Lutfen en az 1 rakam giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lutfen en az bir sembol giriniz."
+			};
+		}
+	}
+}
